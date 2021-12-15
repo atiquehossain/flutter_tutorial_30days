0eb9b66f7f94261f1f23bf16866ec7ae12c46b03
atique.hossain <atique.hossain@savethechildren.org>
2021-12-15 12:43:45 +0600

    dayOne

---
+++ lib/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget {
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int days=30;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title"),
+        leading: Text("leading"),
+        actions: [Text("Action")],
+      ),
+      drawer: Drawer(),
+      body:Center(
+        child: Container(
+          child: Text("We will complete the flutter in $days days" ),
+        ),
+      ),
+    );
+  }
+}
